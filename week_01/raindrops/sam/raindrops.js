44d7edce3e2e31fa98a47b77e949faf848165f48
SamWij <sam.wijesinha@gmail.com>
2017-10-02 15:53:43 +1100

    finished raindrops

---
+++ week_01/raindrops/sam/raindrops.js
@@ -0,0 +1,48 @@
+console.log("raindrops keep falling on my head")
+
+// ## Warmup - Raindrops
+//
+// Write a Javascript program that will take a number (eg `28` or `1755` or `9`, etc) and output the following:
+//
+// - If the number contains 3 as a factor, output 'Pling'.
+// - If the number contains 5 as a factor, output 'Plang'.
+// - If the number contains 7 as a factor, output 'Plong'.
+// - If the number does not contain 3, 5, or 7 as a factor, output the number as a string.
+//
+// ## Examples
+// - 28 has 7 as a factor.
+//   - In raindrop-speak, this would be a simple "Plong".
+// - 1755 has 3 and 5 as factors.
+//   - In raindrop-speak, this would be a "PlingPlang".
+// - 34 has neither 3, 5 nor 7 as a factor.
+//   - Raindrop-speak doesn't know what to make of that,
+//     so it just goes with the straightforward "34".
+var result = [];
+
+function raindrops(number) {
+  result = [];
+
+  if (number % 3 === 0) {
+    result.push("Pling")
+  }
+
+  if (number % 5 === 0) {
+    result.push("Plang")
+  }
+
+  if  (number % 7 === 0){
+    result.push("Plong")
+  }
+
+  if ((number % 3 != 0)  &&  (number % 5 != 0) && (number != 7)) {
+    result.push(number)
+  }
+
+  console.log(result)
+}
+
+
+
+// raindrops(34);
+// raindrops(28);
+raindrops(1755);
